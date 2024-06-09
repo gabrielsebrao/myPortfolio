7f061817f9bdbf996acd55b5affcf711faa23d50
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-06-09 13:05:44 -0300

    remove: undo last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import Body from "./components/Body";
-import CdPlayer from "./components/CdPlayer";
 import Header from "./components/Header";
-import './App.css'
 
 export default function App() {
   return (
     <>
       <Header />
       <Body />
-      <CdPlayer />
     </>
   )
 }
