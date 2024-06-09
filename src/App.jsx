0d9f25d6e3ee91255980db948d611e479284bfee
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-06-09 13:35:41 -0300

    feat: add basic prototype of CD player

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Body from "./components/Body";
 import Header from "./components/Header";
+import CdPlayer from "./components/CdPlayer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Body />
+      <CdPlayer />
     </>
   )
 }
